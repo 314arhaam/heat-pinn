6718d5bd544b74d03aff5903617368716694967f
Parham Abbasi <prmbas@gmail.com>
2026-05-25 22:19:43 +0330

    replaces trained model with orig

--- heat-pinn-cli/commands/train.py
+++ heat-pinn-cli/commands/train.py
@@ -21,5 +21,5 @@ def cmd_train(args):
         every=every
         )
     print(results)
-    joblib.dump(trained_model, pathlib.Path("data/models", f"{model.name}-trained.joblib"))
+    joblib.dump(trained_model, args.model)
     return 0
