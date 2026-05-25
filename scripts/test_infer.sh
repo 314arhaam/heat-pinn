2cd08fdb174fd5de977ac803fcf105367589f238
Parham Abbasi <prmbas@gmail.com>
2026-05-25 20:28:36 +0330

    inference test script

---
+++ scripts/test_infer.sh
@@ -0,0 +1,4 @@
+python3 heat-pinn-cli/heat.py infer \
+    --data data/geometry/square/domain_data.parquet \
+    --model data/models/PINN-DNN-2-1-1-10-50802cd8-c014-4b85-9465-815580a225f1-trained.joblib \
+    --output data/res1.parquet
