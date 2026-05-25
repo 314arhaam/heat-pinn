7a5c208ddd21ada8464d38b76cbf0a17e2ca3200
Parham Abbasi <prmbas@gmail.com>
2026-05-25 22:37:18 +0330

    more epochs

--- scripts/test_train.sh
+++ scripts/test_train.sh
@@ -2,5 +2,5 @@ python3 heat-pinn-cli/heat.py train \
     --domain data/geometry/square/domain_data.parquet \
     --boundary data/geometry/square/boundary_data.parquet \
     --model test_model.joblib \
-    --epochs 20 \
-    --every 2
+    --epochs 1000 \
+    --every 100
