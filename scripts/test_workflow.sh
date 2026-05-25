b376c3477509be1861bf78e0d986c1d1eff4aba5
Parham Abbasi <prmbas@gmail.com>
2026-05-26 01:02:54 +0330

    workflows for test and prod

--- scripts/test_workflow.sh
+++ scripts/test_workflow.sh
@@ -1,14 +1,21 @@
-mkdir -p validation/rod
+mkdir -p validation
 
-GEOM_PATH="data/geometry/rod"
-MODEL_NAME="validation/rod/model_000.joblib"
-INFER_OUT="validation/rod/inference_result.parquet"
+GEOM_PATH="validation"
+MODEL_NAME="validation/model_000.joblib"
+INFER_OUT="validation/inference_result.parquet"
+
+echo "[*] BUILD GEOMETRY"
+python3 utils/square.py \
+  --data-per-boundary 25 \
+  --Nc 10000 \
+  --output-path "${GEOM_PATH}" \
+  --bc-values 0 0.5 0.3 1
 
 echo "[*] BUILD MODEL"
 python3 heat-pinn-cli/heat.py build \
     --in-shape 2 \
     --out-shape 1 \
-    --n-hidden-layers 5 \
+    --n-hidden-layers 1 \
     --neuron-per-layer 10 \
     --actfun tanh \
     --name "${MODEL_NAME}"
