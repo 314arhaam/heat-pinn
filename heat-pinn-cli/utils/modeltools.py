d0fb13f2cd7d6440ba32b7d930aca082e37c2310
Parham Abbasi <prmbas@gmail.com>
2026-05-27 03:58:06 +0330

    renamed

--- heat-pinn-cli/utils/modeltools.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-tf.keras.backend.set_floatx("float64")
-
-def model_wrapper(model):
-    @tf.function
-    def u(x, y):
-        return model(tf.concat([x, y], axis=1))
-    return u
